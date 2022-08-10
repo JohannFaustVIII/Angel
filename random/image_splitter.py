63edc2c0d7e52c9f53fbfc7159bdb996763c7afb
Faust VIII <joh.faustviii@gmail.com>
2022-08-10 23:19:00 +0200

    Update image splitter to read command line arguments

--- random/image_splitter.py
+++ random/image_splitter.py
@@ -1,14 +1,25 @@
 from PIL import Image
 from itertools import product
 import os
+import sys
 
-def tile(filename, dir_in, dir_out, d):
+def tile(filename, dir_out, d):
     name, ext = os.path.splitext(filename)
-    img = Image.open(os.path.join(dir_in, filename))
+    img = Image.open(filename)
     w, h = img.size
     
     grid = product(range(0, h-h%d, d), range(0, w-w%d, d))
     for i, j in grid:
         box = (j, i, j+d, i+d)
-        out = os.path.join(dir_out, f'{name}_{i}_{j}{ext}')
+        out = os.path.join(dir_out, f'result_{i}_{j}{ext}')
+        print(out)
         img.crop(box).save(out)
+
+input = sys.argv[1]
+dir_out = sys.argv[2]
+d = 600
+
+print(input)
+print(dir_out)
+
+tile(input, dir_out, 600)
