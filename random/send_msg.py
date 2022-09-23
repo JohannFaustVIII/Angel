8cf1b0ef7a0865f9ffc5576339619544a3b837f8
Faust VIII <joh.faustviii@gmail.com>
2022-09-23 21:05:50 +0200

    Add message and repeat customisable

--- random/send_msg.py
+++ random/send_msg.py
@@ -1,11 +1,17 @@
 import pyautogui as pg
 import time
+import sys
 
+if __name__ == "__main__":
+    
+    print(len(sys.argv))
+    message = sys.argv[1]
+    repeat = int(sys.argv[2])    
 
-time.sleep(10)
+    time.sleep(10)
 
-for i in range(0,100):
-    pg.write('Message')
-    time.sleep(0.25)
-    pg.press('Enter')
-    time.sleep(0.25)
+    for i in range(0,repeat):
+        pg.write(message)
+        time.sleep(0.25)
+        pg.press('Enter')
+        time.sleep(0.25)
