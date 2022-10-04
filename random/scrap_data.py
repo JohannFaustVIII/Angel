cc6c16ea1e95ea3484be7524113020f35ef721c1
Faust VIII <joh.faustviii@gmail.com>
2022-10-04 10:19:53 +0200

    Refactor code of scrapping data

--- random/scrap_data.py
+++ random/scrap_data.py
@@ -1,4 +1,4 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -7,22 +7,50 @@ from webdriver_manager.chrome import ChromeDriverManager
 def get_text(text : str) -> str:
     return text if text else 'No data'
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-driver.get('https://www.worldometers.info/coronavirus/')
+def get_page_to_parse() -> BeautifulSoup:
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get('https://www.worldometers.info/coronavirus/')
+    return BeautifulSoup(driver.page_source, 'html.parser')
 
-soup = BeautifulSoup(driver.page_source, 'html.parser')
+def get_rows_with_countries(page : BeautifulSoup) -> list[Tag]:
+    return [t for t in page.find_all("tr") if t.find_all("a")]
 
-nested = [t for t in soup.find_all("tr") if t.find_all("a")]
+def map_column_to_text(row : Tag) -> tuple[str, bool]:
+    columns = row.find_all("td")
+    index = columns[0].text
+    country = columns[1].text
+    infected = get_text(columns[3].text)
+    dead = get_text(columns[5].text)
 
-for nes in nested[:100]:
-    tds = nes.find_all("td")
-    text = f"{tds[0].text:>4}\t {tds[1].text:>30}\t {get_text(tds[3].text):>12}\t {get_text(tds[5].text):>12}"
-    print(text)
-    # print(nested)
-    if tds[1].text == "Poland":
-        poland_stats = text
+    message = f"{index:>4}\t {country:>30}\t {infected:>12}\t {dead:>12}"
+    isPoland = country == "Poland"
 
-print(f"Poland stats:\n\n{poland_stats}")
+    return message, isPoland
 
-driver.close()
+def print_countries_stats(countries : list[tuple[str, bool]]):
+    keep_text = lambda country : country[0]
+    countries_stats = list(map(keep_text, countries))
+
+    for stat in countries_stats:
+        print(stat)
+
+def print_poland_stats(countries : list[tuple[str, bool]]):
+    find_poland = lambda country : country[1]
+    keep_text = lambda country : country[0]
+
+    poland_row = list(map(keep_text, list(filter(find_poland, countries))))
+
+    print(f"\nPoland stats:\n{poland_row[0]}")
+
+
+if __name__ == "__main__":
+    page = get_page_to_parse()
+    countries = get_rows_with_countries(page)
+    
+    countries_rows = list(map(map_column_to_text, countries))
+
+    print_countries_stats(countries_rows)
+    print_poland_stats(countries_rows)
+
+    
 
