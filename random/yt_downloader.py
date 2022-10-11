bcff78251e6d0300116fe5f2d06e8e1cd7a8d253
Faust VIII <joh.faustviii@gmail.com>
2022-10-11 09:32:45 +0200

    Refactor downloading yt videos

--- random/yt_downloader.py
+++ random/yt_downloader.py
@@ -1,15 +1,13 @@
 from pytube import YouTube
 import os
+import sys
 
-def download(link : str, output_directory : str, file_name : str, extension : str):
+def download(link : str, output_directory : str, file_name : str, music_only : bool):
     yt = YouTube(url=link)
-    if extension == "mp3":
+    if music_only:
         yt = yt.streams.filter(only_audio=True).first()
-    elif extension == "mp4":
-        yt = yt.streams.get_highest_resolution()
     else:
-        print("Wrong extension passed")
-        return
+        yt = yt.streams.get_highest_resolution()
 
     try:
         out_file = yt.download(output_path=output_directory)
@@ -18,16 +16,24 @@ def download(link : str, output_directory : str, file_name : str, extension : st
         print(e)
         return
 
-    new_file = output_directory + os.sep + file_name + "." + extension
+    new_file = output_directory + os.sep + file_name + ".mp4"
     os.rename(out_file, new_file)
     
     print("Download finished.")
 
-link = input("Download URL: ")
-extension = input("As mp3 or mp4? ") # this mp3 is redundant as music can be in mp4 too? should be slightly different
-if extension in ["mp3", "mp4"]:
+if __name__ == "__main__":
+    music_options = ["m", "music"]
+    video_options = ["v", "video"]
+
+    link = input("Download URL: ")
+    extension = input("As video or music [m/v]:").lower()
+
+    if extension not in music_options and extension not in video_options:
+        print("Wrong extension")
+        sys.exit(0)
+
     output_dir = input("Output directory: ")
     output_file = input("File name: ")
-    download(link, output_dir, output_file, extension)
-else:
-    print("Wrong extension")
+    download(link, output_dir, output_file, extension in music_options)
+
+    
