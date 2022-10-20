d8346418a0ebf1287203ad1b0f76aabfce80e4e7
Faust VIII <joh.faustviii@gmail.com>
2022-10-20 15:42:21 +0200

    AOC 2021 - D7

---
+++ aoc/2021/d7.py
@@ -0,0 +1,21 @@
+with open("aoc/2021/d7.input") as file:
+    line = file.readline()
+
+    positions = list(map(lambda x: int(x), line.strip().split(",")))
+
+print(positions)
+print(min(positions))
+print(max(positions))
+
+def finder(positions : list[int], formula) -> int:
+    return min([sum([formula(i, position) for position in positions]) for i in range(max(positions)+1)])
+
+def part1_formula(position : int, target : int) -> int:
+    return abs(target-position)
+
+def part2_formula(position : int, target : int) -> int:
+    count = abs(target-position)
+    return int((1 + count) * count / 2)
+
+print(finder(positions,part1_formula))
+print(finder(positions,part2_formula))
